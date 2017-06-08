8726706f1f44b2074164ea9657a5b9708127c4e6
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2017-06-08 17:40:38 +0300

    Fixed image upload and added image preview in widget form. closes #1

--- app/code/community/TM/EasyLightbox/Block/Adminhtml/Widget/Image.php
+++ app/code/community/TM/EasyLightbox/Block/Adminhtml/Widget/Image.php
@@ -1,15 +1,20 @@
 <?php
 
-class TM_EasyLightbox_Block_Adminhtml_Widget_Image
-    extends Mage_Adminhtml_Block_Template implements Varien_Data_Form_Element_Renderer_Interface
+class TM_EasyLightbox_Block_Adminhtml_Widget_Image extends Mage_Adminhtml_Block_Template
+    implements Varien_Data_Form_Element_Renderer_Interface
 {
+    protected $_template = 'tm/easylightbox/widget/image.phtml';
+
+    protected $_element = null;
+
+    public function getElement()
+    {
+        return $this->_element;
+    }
+
     public function render(Varien_Data_Form_Element_Abstract $element)
     {
-        return '<tr>
-            <td class="label"><label>' . $this->__('Image') . '</label></td>
-            <td class="value" id="file-easylightbox-wrapper">
-                <input type="file" name="image" onchange="return AIM.submit($(this).up(\'form\'), {action: \'' . $this->getUrl('adminhtml/easylightbox_image/save') . '\'})">
-            </td>
-        </tr>';
+        $this->_element = $element;
+        return $this->toHtml();
     }
 }
